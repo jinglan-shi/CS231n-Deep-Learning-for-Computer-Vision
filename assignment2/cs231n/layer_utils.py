7754e5a2770e19a594ea002a38925320547eb27c
Jinglan <80923736+jinglan-shi@users.noreply.github.com>
2023-02-12 00:10:17 +0100

    Add files via upload

---
+++ assignment2/cs231n/layer_utils.py
@@ -0,0 +1,169 @@
+from .layers import *
+from .fast_layers import *
+
+
+def affine_relu_forward(x, w, b):
+    """Convenience layer that performs an affine transform followed by a ReLU.
+
+    Inputs:
+    - x: Input to the affine layer
+    - w, b: Weights for the affine layer
+
+    Returns a tuple of:
+    - out: Output from the ReLU
+    - cache: Object to give to the backward pass
+    """
+    a, fc_cache = affine_forward(x, w, b)
+    out, relu_cache = relu_forward(a)
+    cache = (fc_cache, relu_cache)
+    return out, cache
+
+def affine_relu_backward(dout, cache):
+    """Backward pass for the affine-relu convenience layer.
+    """
+    fc_cache, relu_cache = cache
+    da = relu_backward(dout, relu_cache)
+    dx, dw, db = affine_backward(da, fc_cache)
+    return dx, dw, db
+
+# *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+pass
+
+# *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+def conv_relu_forward(x, w, b, conv_param):
+    """A convenience layer that performs a convolution followed by a ReLU.
+
+    Inputs:
+    - x: Input to the convolutional layer
+    - w, b, conv_param: Weights and parameters for the convolutional layer
+
+    Returns a tuple of:
+    - out: Output from the ReLU
+    - cache: Object to give to the backward pass
+    """
+    a, conv_cache = conv_forward_fast(x, w, b, conv_param)
+    out, relu_cache = relu_forward(a)
+    cache = (conv_cache, relu_cache)
+    return out, cache
+
+
+def conv_relu_backward(dout, cache):
+    """Backward pass for the conv-relu convenience layer.
+    """
+    conv_cache, relu_cache = cache
+    da = relu_backward(dout, relu_cache)
+    dx, dw, db = conv_backward_fast(da, conv_cache)
+    return dx, dw, db
+
+
+def conv_bn_relu_forward(x, w, b, gamma, beta, conv_param, bn_param):
+    """Convenience layer that performs a convolution, a batch normalization, and a ReLU.
+
+    Inputs:
+    - x: Input to the convolutional layer
+    - w, b, conv_param: Weights and parameters for the convolutional layer
+    - pool_param: Parameters for the pooling layer
+    - gamma, beta: Arrays of shape (D2,) and (D2,) giving scale and shift
+      parameters for batch normalization.
+    - bn_param: Dictionary of parameters for batch normalization.
+
+    Returns a tuple of:
+    - out: Output from the pooling layer
+    - cache: Object to give to the backward pass
+    """
+    a, conv_cache = conv_forward_fast(x, w, b, conv_param)
+    an, bn_cache = spatial_batchnorm_forward(a, gamma, beta, bn_param)
+    out, relu_cache = relu_forward(an)
+    cache = (conv_cache, bn_cache, relu_cache)
+    return out, cache
+
+
+def conv_bn_relu_backward(dout, cache):
+    """Backward pass for the conv-bn-relu convenience layer.
+    """
+    conv_cache, bn_cache, relu_cache = cache
+    dan = relu_backward(dout, relu_cache)
+    da, dgamma, dbeta = spatial_batchnorm_backward(dan, bn_cache)
+    dx, dw, db = conv_backward_fast(da, conv_cache)
+    return dx, dw, db, dgamma, dbeta
+
+
+def conv_relu_pool_forward(x, w, b, conv_param, pool_param):
+    """Convenience layer that performs a convolution, a ReLU, and a pool.
+
+    Inputs:
+    - x: Input to the convolutional layer
+    - w, b, conv_param: Weights and parameters for the convolutional layer
+    - pool_param: Parameters for the pooling layer
+
+    Returns a tuple of:
+    - out: Output from the pooling layer
+    - cache: Object to give to the backward pass
+    """
+    a, conv_cache = conv_forward_fast(x, w, b, conv_param)
+    s, relu_cache = relu_forward(a)
+    out, pool_cache = max_pool_forward_fast(s, pool_param)
+    cache = (conv_cache, relu_cache, pool_cache)
+    return out, cache
+
+
+def conv_relu_pool_backward(dout, cache):
+    """Backward pass for the conv-relu-pool convenience layer.
+    """
+    conv_cache, relu_cache, pool_cache = cache
+    ds = max_pool_backward_fast(dout, pool_cache)
+    da = relu_backward(ds, relu_cache)
+    dx, dw, db = conv_backward_fast(da, conv_cache)
+    return dx, dw, db
+
+def affine_norm_relu_forward(x, w, b, gamma, beta, bn_params, normalization):
+  """
+  Convenience/"sandwich"/helper layer that combines multiple operations into commonly used patterns.
+  Performs affine - batch/layer norm - relu.
+  Inputs:
+  - x: Input to the affine layer
+  - w, b: Weights for the affine layer
+  - gamma, beta: Batchnorm/Layernorm learnable params
+  - bn_params: Batchnorm/Layernorm params
+  - normalization: Are we using Batchnorm or Layernorm?
+  Returns a tuple of:
+  - out: Output from the ReLU
+  - cache: Tuple containing the cache of each layer to give to the backward pass
+  
+  """
+
+  fc_cache, bn_cache, relu_cache = None, None, None
+        
+  # affine layer
+  out, fc_cache = affine_forward(x, w, b)
+
+  # batch/layer norm
+  if normalization == 'batchnorm':
+    out, bn_cache = batchnorm_forward(out, gamma, beta, bn_params)
+  elif normalization == 'layernorm':
+    out, bn_cache = layernorm_forward(out, gamma, beta, bn_params)       
+
+  # relu
+  out, relu_cache = relu_forward(out)
+
+  return out, (fc_cache, bn_cache, relu_cache)
+
+def affine_norm_relu_backward(dout, cache, normalization):
+  fc_cache, bn_cache, relu_cache = cache
+
+  # Relu
+  dout = relu_backward(dout, relu_cache)
+
+  # Batch/layer normalization
+  dgamma, dbeta = None, None
+  if normalization == 'batchnorm':
+    dout, dgamma, dbeta = batchnorm_backward(dout, bn_cache)
+  elif normalization == 'layernorm':
+    dout, dgamma, dbeta = layernorm_backward(dout, bn_cache)
+
+  # Affine layer
+  dx, dw, db = affine_backward(dout, fc_cache)
+
+  return dx, dw, db, dgamma, dbeta
